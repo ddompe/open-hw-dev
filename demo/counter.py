9073ca4b4b822d1e3ed6ef2f65ed3a22dad83137
lasahu <laura-salazar.huezo@hpe.como>
2020-02-25 13:40:05 +0000

    addded comments

--- demo/counter.py
+++ demo/counter.py
@@ -4,25 +4,70 @@ import argparse
 import os
 from migen import *
 
-
-# Simple state machine 
+# This is an example that an state machine using Python Migen 
+######################################################################
+#         _____________
+#        |             |
+#        |             |
+#        |   DEFAULT   |
+#        |_____________|  
+# 
+#              |
+#         _____________
+#        |             |
+#        |   INIT      |  <---------------|                
+#        | counter_b + |                  |
+#        |_____________|                  |
+#                                         |
+#               |  (counter_b==5)?        |
+#         _____________                   |
+#        |             |                  |
+#        |             |                  |
+#        |   WAIT      |                  |
+#        |_____________|                  |
+#                                         |
+#               | push?                   |
+#         _____________                   |
+#        |             |                  |
+#        |   COUNT     |                  |
+#        | counter_a + |                  |
+#        |_____________|                  |
+#                                         |
+#               |  (counter_b==10)?       |
+#         _____________                   |
+#        |             |                  |
+#        |             | _________________|
+#        | COUNT_DONE  |
+#        |_____________|
+#
+######################################################################
+# To declare module use classes with keyword module inside
 class State_Machine(Module):
     def __init__(self):
+        
+        #No reset,clock declaration are needed they are done by default
+        #inputs and outputs are infered
+        
+        #Signal declarations
+        #By default all signals values are zero 
         self.s    = Signal()
         self.init = Signal()
         self.counting = Signal()
+        #Give the reset value at signal declaration
         self.green = Signal(reset=1)
         self.red   = Signal()
         self.push  = Signal()
         self.counter_a = Signal(8)
         self.counter_b = Signal(8)
         
-        #Create 7 diferent Signals
+        #Create 7 diferents Signals: a0, a1,.... a6
         x = Array(Signal(name="a") for i in range(7))
 
+        #Use the function to create a FSM() state machine
         myfsm = FSM()
         self.submodules += myfsm
 
+        # Describe each of the FSM states
         myfsm.act("DEFAULT",
             self.s.eq(1),
             self.red.eq(0),
@@ -65,9 +110,16 @@ class State_Machine(Module):
             NextState("INIT")
         )
         
-
+        #This are fsm functions that allows us give values to signals
+        #depending which state is the State_Machine
+        #as example yellow will high just the cycle before entering to COUNT state
         self.yellow = myfsm.before_entering("COUNT")
         self.be = myfsm.before_entering("FOO")
         self.ae = myfsm.after_entering("FOO")
         self.bl = myfsm.before_leaving("FOO")
         self.al = myfsm.after_leaving("FOO")
+######################################################################
+#    
+#    LASCAS Febreary  2020 
+#
+######################################################################
--- demo/test.py
+++ demo/test.py
@@ -7,7 +7,10 @@ from counter import *
 from migen import *
 from migen.fhdl import verilog
 
-#Test function
+######################################################################
+#   Test function
+# Instead of clock cycles Python uses yield to create concurrency 
+# every yield can see that advance 1 clock cycle 
 def sm_test(dut):
     for i in range(6):
         yield
@@ -17,16 +20,24 @@ def sm_test(dut):
     for i in range(20):
         yield  
         
-
+######################################################################
 # create verilog code
 if __name__ == "__main__":
     dut = State_Machine()
 
     create_verilog = 1
     if(create_verilog):
+        #dut.s, dut.counter_a ... are the inputs/outputs that I want to have in the verilog 
         verilog.convert(dut, {dut.s, dut.counter_a, dut.be, dut.ae, dut.bl, dut.al}).write("my_machine.v")
-
+######################################################################
 # Run the simulation
 if __name__ == "__main__":
     dut = State_Machine()
+    #gtkawave is create from Python code
     run_simulation(dut, sm_test(dut), vcd_name="state_machine.vcd")
+    
+######################################################################
+#    
+#    LASCAS Febreary  2020 
+#
+######################################################################
